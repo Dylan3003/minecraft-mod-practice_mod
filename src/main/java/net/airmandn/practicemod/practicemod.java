78722f79f4c927eeaf0e33a913150515a7a09168
Dylan3003 <dylan.noordzij@gmail.com>
2023-04-03 11:59:58 +0200

    Change practicemod file

--- src/main/java/net/airmandn/practicemod/practicemod.java
+++ src/main/java/net/airmandn/practicemod/practicemod.java
@@ -1,6 +1,7 @@
 package net.airmandn.practicemod;
 
 import com.mojang.logging.LogUtils;
+import net.airmandn.practicemod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,6 +21,8 @@ public class practicemod
     public practicemod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
