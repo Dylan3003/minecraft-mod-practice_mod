b80888d665ba73718d861f65e999ba889bd1026c
Dylan3003 <dylan.noordzij@gmail.com>
2023-04-03 11:59:06 +0200

    Add Items to the ModItems.java class

--- src/main/java/net/airmandn/practicemod/item/ModItems.java
+++ src/main/java/net/airmandn/practicemod/item/ModItems.java
@@ -1,4 +1,23 @@
 package net.airmandn.practicemod.item;
 
+import net.airmandn.practicemod.practicemod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, practicemod.MOD_ID);
+
+    public static final RegistryObject<Item> PIZZA = ITEMS.register("pizza",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRACTICE_TAB)));
+    public static final RegistryObject<Item> RAW_PIZZA = ITEMS.register("raw_pizza",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.PRACTICE_TAB)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
