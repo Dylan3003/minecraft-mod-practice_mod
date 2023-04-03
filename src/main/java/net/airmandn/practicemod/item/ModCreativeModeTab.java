488df8d7593e443c9f27fb874bcbcbdae9eede56
Dylan3003 <dylan.noordzij@gmail.com>
2023-04-03 12:01:06 +0200

    Add seperate creative mode tab

---
+++ src/main/java/net/airmandn/practicemod/item/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package net.airmandn.practicemod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab PRACTICE_TAB = new CreativeModeTab("practicetab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.PIZZA.get());
+        }
+    };
+}
